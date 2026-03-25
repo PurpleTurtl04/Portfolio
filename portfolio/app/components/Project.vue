ca5720d84bbe03b8373f58a8b9613d950f23c3ed
Nick Edwards <nledwa01@gmail.com>
2026-03-25 11:21:05 -0500

    Continued layout adjustments to Landing page Project section. Implemented Project child component. Set max width for Project and About me sections

--- portfolio/app/components/Project.vue
+++ portfolio/app/components/Project.vue
@@ -1,55 +1,55 @@
 <template>
     <div
-        class="container mx-auto mt-2 grid grid-cols-1 items-center justify-center gap-6 px-6 lg:grid-cols-2"
+        class="container mx-auto grid max-w-7xl grid-cols-1 items-center justify-center gap-6 pb-6 lg:mt-4 lg:grid-cols-2"
     >
-        <div class="flex flex-col items-center">
-            <div class="flex flex-col items-center gap-2 lg:items-start">
+        <div
+            :class="`flex flex-col items-center ${id % 2 == 0 ? 'lg:order-1' : 'lg:order-2'}`"
+        >
+            <div
+                class="mt-4 flex flex-col items-center gap-2 lg:mt-0 lg:items-start"
+            >
                 <h3 class="text-2xl lg:text-3xl">{{ name }}</h3>
                 <div class="flex gap-1 lg:gap-1.5">
                     <div
-                        class="badge badge-sm md:badge-md badge-soft badge-warning"
+                        v-for="tool in tools"
+                        :class="`badge badge-sm md:badge-md badge-soft ${tool.color}`"
                     >
-                        Javascript
-                    </div>
-                    <div
-                        class="badge badge-sm md:badge-md badge-soft badge-info"
-                    >
-                        React
-                    </div>
-                    <div
-                        class="badge badge-sm md:badge-md badge-soft badge-secondary"
-                    >
-                        Tailwindcss
-                    </div>
-                    <div
-                        class="badge badge-sm md:badge-md badge-soft badge-success"
-                    >
-                        Supabase
+                        {{ tool.tool }}
                     </div>
                 </div>
-                <p class="mt-3 max-w-lg text-center lg:text-left">
+                <p class="mt-2 max-w-lg text-center lg:text-left">
                     {{ description }}
                 </p>
-                <div class="flex justify-center gap-2 pt-6 lg:justify-start">
-                    <button
-                        class="btn btn-primary bg-primary/90 tracking-wider"
-                    >
-                        View Project
-                    </button>
-                    <button class="btn btn-outline btn-primary tracking-wider">
-                        Github Repo
-                    </button>
+                <div
+                    class="flex justify-center gap-2 pt-3 lg:justify-start lg:pt-6"
+                >
+                    <a :href="url" target="_blank">
+                        <button
+                            class="btn btn-primary bg-primary/90 tracking-wider"
+                        >
+                            View Project
+                        </button>
+                    </a>
+                    <a :href="github" target="_blank">
+                        <button
+                            class="btn btn-outline btn-primary tracking-wider"
+                        >
+                            Github Repo
+                        </button>
+                    </a>
                 </div>
             </div>
         </div>
-        <div class="card mx-auto flex max-w-lg justify-center">
+        <div
+            :class="`card mx-auto flex max-w-lg justify-center ${id % 2 == 0 ? 'lg:order-2' : 'lg:order-1'}`"
+        >
             <NuxtImg :src="image" :alt="name" class="rounded-2xl" />
         </div>
     </div>
 </template>
 
 <script setup>
-    defineProps({
+    const props = defineProps({
         id: Number,
         name: String,
         tools: {
--- portfolio/app/components/landing/ProjectSection.vue
+++ portfolio/app/components/landing/ProjectSection.vue
@@ -14,65 +14,8 @@
             v-for="(project, index) in projectListLimited"
             :key="project.id"
             :="project"
+            :index="index"
         />
-        <!-- <div
-            class="container mx-auto mt-2 grid grid-cols-1 items-center justify-center gap-6 px-6 lg:grid-cols-2"
-        >
-            <div class="flex flex-col items-center">
-                <div class="flex flex-col items-center gap-2 lg:items-start">
-                    <h3 class="text-2xl lg:text-3xl">Beckaroo & The Zoo</h3>
-                    <div class="flex gap-1 lg:gap-1.5">
-                        <div
-                            class="badge badge-sm md:badge-md badge-soft badge-warning"
-                        >
-                            Javascript
-                        </div>
-                        <div
-                            class="badge badge-sm md:badge-md badge-soft badge-info"
-                        >
-                            React
-                        </div>
-                        <div
-                            class="badge badge-sm md:badge-md badge-soft badge-secondary"
-                        >
-                            Tailwindcss
-                        </div>
-                        <div
-                            class="badge badge-sm md:badge-md badge-soft badge-success"
-                        >
-                            Supabase
-                        </div>
-                    </div>
-                    <p class="mt-3 max-w-lg text-center lg:text-left">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Duis non nisi eget ipsum lacinia varius et maximus
-                        magna. Sed hendrerit purus imperdiet elementum
-                        condimentum. Mauris cursus sagittis iaculis.
-                    </p>
-                    <div
-                        class="flex justify-center gap-2 pt-6 lg:justify-start"
-                    >
-                        <button
-                            class="btn btn-primary bg-primary/90 tracking-wider"
-                        >
-                            View Project
-                        </button>
-                        <button
-                            class="btn btn-outline btn-primary tracking-wider"
-                        >
-                            Github Repo
-                        </button>
-                    </div>
-                </div>
-            </div>
-            <div class="card mx-auto flex max-w-lg justify-center">
-                <NuxtImg
-                    src="/images/Beckaroo.png"
-                    alt="Landing page for the website Beckaroo and the Zoo with a picture of a Corgi."
-                    class="rounded-2xl"
-                />
-            </div>
-        </div> -->
     </div>
 </template>
 
