c04ae616037941c5dbe183233541f42d1d4a66da
Nick Edwards <nledwa01@gmail.com>
2026-03-25 15:41:49 -0500

    Modified Project component to utilize daisyUI image gallery feature. Added additional photos and changed structure of projects.json file.

--- portfolio/app/components/Project.vue
+++ portfolio/app/components/Project.vue
@@ -1,9 +1,9 @@
 <template>
     <div
-        class="container mx-auto grid max-w-7xl grid-cols-1 items-center justify-center gap-6 pb-6 lg:mt-4 lg:grid-cols-2"
+        class="container mx-auto grid max-w-7xl grid-cols-1 items-center justify-center gap-6 pb-8 lg:mt-4 lg:grid-cols-2"
     >
         <div
-            :class="`flex flex-col items-center ${id % 2 == 0 ? 'lg:order-1' : 'lg:order-2'}`"
+            :class="`flex flex-col items-center ${(index + 1) % 2 == 0 ? 'lg:order-1' : 'lg:order-2'}`"
         >
             <div
                 class="mt-4 flex flex-col items-center gap-2 lg:mt-0 lg:items-start"
@@ -11,10 +11,10 @@
                 <h3 class="text-2xl lg:text-3xl">{{ name }}</h3>
                 <div class="flex gap-1 lg:gap-1.5">
                     <div
-                        v-for="tool in tools"
-                        :class="`badge badge-sm md:badge-md badge-soft ${tool.color}`"
+                        v-for="tech in techStack"
+                        :class="`badge badge-sm md:badge-md badge-soft ${tech.color}`"
                     >
-                        {{ tool.tool }}
+                        {{ tech.name }}
                     </div>
                 </div>
                 <p class="mt-2 max-w-lg text-center lg:text-left">
@@ -41,9 +41,17 @@
             </div>
         </div>
         <div
-            :class="`card mx-auto flex max-w-lg justify-center ${id % 2 == 0 ? 'lg:order-2' : 'lg:order-1'}`"
+            :class="`card mx-auto flex max-w-lg justify-center ${(index + 1) % 2 == 0 ? 'lg:order-2' : 'lg:order-1'}`"
         >
-            <NuxtImg :src="image" :alt="name" class="rounded-2xl" />
+            <div class="hover-gallery">
+                <NuxtImg
+                    v-for="(image, index) in images"
+                    :key="index"
+                    :src="image"
+                    :alt="`${name} screenshot ${index + 1}`"
+                    class="rounded-2xl"
+                />
+            </div>
         </div>
     </div>
 </template>
@@ -52,15 +60,15 @@
     const props = defineProps({
         id: Number,
         name: String,
-        tools: {
+        techStack: {
             type: Object,
-            tool: String,
+            name: String,
             color: String,
         },
         description: String,
         url: String,
         github: String,
-        image: String,
+        images: Array,
         index: Number,
     });
 </script>
