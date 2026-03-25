47ca5547eba9ac4566e348649af1d6e892d2fb04
Nick Edwards <nledwa01@gmail.com>
2026-03-25 15:57:34 -0500

    Updated projects page to with logic to display all projects

--- portfolio/app/pages/projects.vue
+++ portfolio/app/pages/projects.vue
@@ -1,14 +1,27 @@
 <template>
-    <div class="p-6">
-        <h1 class="text-3xl font-bold">Projects</h1>
-
-        <div class="grid gap-4 mt-4 md:grid-cols-2">
-            <div class="card bg-base-100 shadow-xl">
-                <div class="card-body">
-                    <h2 class="card-title">Project One</h2>
-                    <p>Short description of your project.</p>
-                </div>
-            </div>
+    <div class="container mx-auto mt-15 px-6">
+        <div class="flex flex-col items-center">
+            <h2 class="text-center text-3xl lg:text-4xl">Portfolio</h2>
+            <NuxtImg
+                src="/images/Accent2.png"
+                width="10"
+                height="75"
+                alt="A line with a circle at the bottom"
+                class="scale-75"
+            />
         </div>
+        <Project
+            v-for="(project, index) in projects"
+            :key="project.id"
+            :="project"
+            :index="index"
+        />
     </div>
 </template>
+
+<script setup>
+    import { ref } from 'vue';
+    import projects from '~/data/projects.json';
+
+    const projectList = ref(projects);
+</script>
