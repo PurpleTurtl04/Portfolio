6ea3008c70b074998a3f5d888bc0a7dbbd760c9d
Nick Edwards <nledwa01@gmail.com>
2026-03-27 14:05:51 -0500

    Added information to Footer contact images. Installed Pinia

--- portfolio/app/components/Footer.vue
+++ portfolio/app/components/Footer.vue
@@ -3,25 +3,34 @@
         class="mt-30 flex flex-col items-center justify-center bg-white/10 lg:mt-40"
     >
         <h2 class="mt-6 pb-4 text-2xl">Nick Edwards</h2>
-        <div class="flex gap-4 pb-6">
-            <NuxtImg
-                src="/images/Phone.svg"
-                width="25"
-                height="25"
-                alt="A phone symbol"
-            />
-            <NuxtImg
-                src="/images/Mail.svg"
-                width="25"
-                height="25"
-                alt="A mail envelope"
-            />
-            <NuxtImg
-                src="/images/LinkedIn.svg"
-                width="25"
-                height="25"
-                alt="The LinkedIn logo"
-            />
+        <div class="flex items-center justify-center gap-4 pb-6">
+            <a href="tel:+15022996425" title="Click to call: +1 (502) 299-6425">
+                <NuxtImg
+                    src="/images/Phone.svg"
+                    width="25"
+                    height="25"
+                    alt="A phone symbol"
+                />
+            </a>
+            <a href="mailto:nledwa01@gmail.com">
+                <NuxtImg
+                    src="/images/Mail.svg"
+                    width="25"
+                    height="25"
+                    alt="A mail envelope"
+                />
+            </a>
+            <a
+                href="https://www.linkedin.com/in/nickedwards4186/"
+                target="blank"
+            >
+                <NuxtImg
+                    src="/images/LinkedIn.svg"
+                    width="25"
+                    height="25"
+                    alt="The LinkedIn logo"
+                />
+            </a>
         </div>
         <p class="mb-4">&copy; 2026 Nick Edwards. All rights reserved.</p>
     </footer>
--- portfolio/nuxt.config.ts
+++ portfolio/nuxt.config.ts
@@ -5,8 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
     devtools: { enabled: true },
-    modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/fonts'],
-    // modules: ['@nuxt/eslint', '@nuxt/image'],
+    modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/fonts', '@pinia/nuxt'],
     vite: {
         plugins: [tailwindcss() as any],
     },
